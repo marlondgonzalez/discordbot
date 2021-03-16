c1fda28525529bd4536244462da928676a814409
marlondgonzalez <48197054+marlondgonzalez@users.noreply.github.com>
2021-03-15 18:33:26 -0700

    Update quenchbot.py

--- bot/quenchbot.py
+++ bot/quenchbot.py
@@ -1,19 +1,19 @@
-import discord
 import os
-# import psycopg2
+import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 # DATABASE_URL = os.getenv("DATABASE_URL")
 
-client = discord.Client()
+client = commands.Bot(command_prefix = "!")
 # conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 # cursor = conn.cursor()
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print(f"We have logged in as {client.user}")
 
 @client.event
 async def on_message(message):

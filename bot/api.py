83a6d357b36d4123e6ba1e4e6abea20056a65e80
marlondgonzalez <48197054+marlondgonzalez@users.noreply.github.com>
2021-03-23 23:32:06 -0700

    Update api.py

--- bot/api.py
+++ bot/api.py
@@ -27,9 +27,11 @@ class RegisterTwitchStreamer(TwitchAPI):
     def createTwitchAppToken(self):
         # url = f"https://id.twitch.tv/oauth2/token?client_id={TWITCH_CLIENT_ID}&client_secret={TWITCH_CLIENT_SECRET}&grant_type=client_credentials&scope=<space-separated list of scopes>"
         url = f"https://id.twitch.tv/oauth2/token?client_id={self.clientID}&client_secret={self.clientSecret}&grant_type=client_credentials"
+        statusCode = response.status_code
+        responseBody = response.text
+        print(responseBody)
         response = requests.post(url).json()
-        print(response)
-        if response.status_code == 200:
+        if statusCode == 200:
             self.token = response["access_token"]
             return self.token
         else:

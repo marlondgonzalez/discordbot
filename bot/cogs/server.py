67be89fb330348218192fcb99f12788726768393
marlondgonzalez <48197054+marlondgonzalez@users.noreply.github.com>
2021-03-24 02:56:04 -0700

    Update server.py

--- bot/cogs/server.py
+++ bot/cogs/server.py
@@ -43,10 +43,9 @@ class Server(commands.Cog):
                 print(actualsignature)
                 print(expectedsignature)
                 print(actualsignature == expectedsignature)
-                print(dir(request))
-                print(request.keys())
+                print(request.json())
                 challenge = request.content.get("challenge")
-                print(request.content.keys())
+
                 return web.Response(text=challenge, status=200)
                 # except:
                 #     data = await request.json()
